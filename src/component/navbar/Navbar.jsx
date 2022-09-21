02063b47b22f1df23a2947e6d68ec08b03d5fc98
Fritzzzz <friedmanliran@gmail.com>
2022-09-21 16:53:05 +0300

    code review

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -1,98 +1,93 @@
-import React from 'react';
-import { NavLink} from 'react-router-dom';
-import styled from 'styled-components';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+import { LOGO, NAVBAR_LINKS } from "../../constants";
 // import { useRef } from 'react';
 
-
-
-
-const Container = styled.div`
-width: 100%;
-height: 10%;
-background-color: #fff;
-display: flex;
-justify-content: space-between;
-align-items: center;
-`
-const Left = styled.div`
-display: flex;
-justify-content: space-around;
-flex: 1;
-align-items: center;
-`
-const Right = styled.div`
-display: flex;
-justify-content: space-around;
-flex: 1;
-align-items: center;
-`
-const Center= styled.div`
-display: flex;
-flex: 2;
-`
-const Logo = styled(NavLink)`
-text-decoration: none;
-font-weight: 900;
-font-size: 20px;
-color: darkblue;
-cursor: pointer;
-`
-const Menu = styled.ul`
-display: flex;
-list-style: none;
-
-`
-const NavbarLinks = styled(NavLink)`
-text-decoration: none;
-font-size: 20px;
-font-weight: bold;
-margin-right: 30px;
-color: gray;
-cursor: pointer;
-&:hover{
-    color: darkblue;
-}
-`
-const ContactlinkButton = styled(NavLink)`
-text-decoration: none;
-font-weight: bold;
-background-color: darkblue;
-cursor: pointer;
-border: 2px solid white;
-color: #fff;
-padding: 10px 20px;
-border-radius: 10px;
-&:hover{
-    background-color: aliceblue;
-    color: darkblue;
-    border: 2px solid darkblue;
-}
-`
-
-const  Navbar = () =>{
+const Navbar = () => {
+  const { HOME, ABOUT, PROJECT, CONTACT } = NAVBAR_LINKS;
 
   return (
     <Container>
-
       <Left>
-      <Logo to="/">Uday</Logo>
+        <Logo to="/">{LOGO}</Logo>
       </Left>
 
       <Center>
-      <Menu>
-        <NavbarLinks to="/">Home</NavbarLinks>
-        <NavbarLinks to="/About">About</NavbarLinks>
-        <NavbarLinks to="/project">Project</NavbarLinks>
-      </Menu>
+        <Menu>
+          <NavbarLinks to="/">{HOME}</NavbarLinks>
+          <NavbarLinks to="/About">{ABOUT}</NavbarLinks>
+          <NavbarLinks to="/project">{PROJECT}</NavbarLinks>
+        </Menu>
       </Center>
 
       <Right>
-      <ContactlinkButton to="/Contact">Contact</ContactlinkButton>
+        <ContactlinkButton to="/Contact">{CONTACT}</ContactlinkButton>
       </Right>
-
     </Container>
-  )
-}
+  );
+};
 
+export default Navbar;
 
-export default Navbar
+const Container = styled.div`
+  width: 100%;
+  height: 10%;
+  background-color: #fff;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+const Left = styled.div`
+  display: flex;
+  justify-content: space-around;
+  flex: 1;
+  align-items: center;
+`;
+const Right = styled.div`
+  display: flex;
+  justify-content: space-around;
+  flex: 1;
+  align-items: center;
+`;
+const Center = styled.div`
+  display: flex;
+  flex: 2;
+`;
+const Logo = styled(NavLink)`
+  text-decoration: none;
+  font-weight: 900;
+  font-size: 20px;
+  color: darkblue;
+  cursor: pointer;
+`;
+const Menu = styled.ul`
+  display: flex;
+  list-style: none;
+`;
+const NavbarLinks = styled(NavLink)`
+  text-decoration: none;
+  font-size: 20px;
+  font-weight: bold;
+  margin-right: 30px;
+  color: gray;
+  cursor: pointer;
+  &:hover {
+    color: darkblue;
+  }
+`;
+const ContactlinkButton = styled(NavLink)`
+  text-decoration: none;
+  font-weight: bold;
+  background-color: darkblue;
+  cursor: pointer;
+  border: 2px solid white;
+  color: #fff;
+  padding: 10px 20px;
+  border-radius: 10px;
+  &:hover {
+    background-color: aliceblue;
+    color: darkblue;
+    border: 2px solid darkblue;
+  }
+`;
---
+++ src/constants.js
@@ -0,0 +1,13 @@
+export const RESUME_DESCRIPTION =
+  "          I'm a Junior Full Stack Developer who loves creative problem solving and passionate about emerging technologies. I'm a very fast learner who is looking for my first job as a developer. Experienced with: JavaScript, React.js, Node.js, MongoDB, HTML, CSS,APIs,";
+export const JOB_TITLE = "Frontend Developer";
+export const VIEW_PROJECT = " View Project";
+export const CV = "CV";
+export const LOGO = "Uday";
+
+export const NAVBAR_LINKS = {
+  HOME: "Home",
+  ABOUT: "About",
+  PROJECT: "Project",
+  CONTACT: "Contact", //...bad name
+};
